7ea115ae6bc5edc13332c28738fc697e88501fc8
strangerbot <lyfebotstranger@gmail.com>
2025-04-14 06:49:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "918076189643"
+global.ownername = "Stranger"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
+global.ownernumber = '918076189643'  //creator number
+global.ownername = 'Stranger' //owner name
 global.botname = 'ELITE-PRO-V1' //name of the bot
 
 
